d2c88a031721b3c8c2609bb317955ede333119a2
Luizfer-Col <luizfercolorado@gmail.com>
2021-10-16 18:47:46 -0500

    added components comics and information

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Home from './components/Home'
+import Detail from './components/Detail';
 
 const Stack = createStackNavigator();
 
@@ -10,6 +11,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detail" component={Detail} />
       </Stack.Navigator>
     </NavigationContainer>
   );
